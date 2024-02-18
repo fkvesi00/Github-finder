78a26b570485990dda36bf318440aab3c9d067d0
fkvesi00 <fkvesi00@fesb.hr>
2024-02-18 19:35:16 +0100

    adding new components and spinner gif

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import UserResults from '../components/users/UserResults'
 
 function Home() {
   return (
     <div className='text-6xl'>
-        Welcome
+      <UserResults />
     </div>
   )
 }
