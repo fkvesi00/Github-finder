2d6474e2b7e96a2c9dc76d3fa61498b7918cf7a6
fkvesi00 <fkvesi00@fesb.hr>
2024-02-26 16:24:52 +0100

    adding axios, and action.js file

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,24 +1,34 @@
 import React,{useState, useContext} from 'react';
 import GithubContext from '../../context/github/GithubContext';
 import AlertContext from '../../context/alert/AlertContext';
+import {searchUsers} from '../../context/github/GithubActions';
 
 function UserSearch() {
     const [text, setText] = useState('')
 
-    const {users, searchUsers, clearUsers} = useContext(GithubContext)
+    const {users, dispatch} = useContext(GithubContext)
     const {setAlert} = useContext(AlertContext)
 
     const handleTextChange = (e) => {
         setText(e.target.value)
     }
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault()
 
         if(text === ''){
             setAlert ('Please enter something', 'error')
         }else {
-            searchUsers(text)
+          dispatch({
+            type: 'SET_LOADING'
+          })
+
+          const users = await searchUsers(text)
+
+          dispatch({
+            type:'GET_USERS',
+            payload:users
+          })
 
             setText('')
         }
@@ -49,7 +59,7 @@ function UserSearch() {
       </div>
       {users.length > 0 && (
         <div className="flex items-end justify-center">
-        <button className='btn btn-ghost btn-lg bg-gray-500' onClick={clearUsers}>
+        <button className='btn btn-ghost btn-lg bg-gray-500' onClick={() =>dispatch({type:'CLEAR_USERS'})}>
           Clear
         </button>
       </div>
--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -3,82 +3,20 @@ import githubReducer from "./GithubReducer";
 
 const GithubContext = createContext()
 
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
-
 export const GithubProvider = ({children}) => {
     const intialState = {
         users: [],
+        repos:[],
         user:{},
         loading: false
     }
 
     const [state, dispatch] = useReducer(githubReducer, intialState)
-
-    //get search results
-    const searchUsers = async (text) => {
-        setLoading()
-
-        const params = new URLSearchParams({
-            q: text
-        })
-
-        const reposnse = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-            headers: {
-                Authorization : `token ${GITHUB_TOKEN}`,
-            },
-        })
-
-        const {items} = await reposnse.json()
-
-
-        dispatch({
-            type:'GET_USERS',
-            payload:items
-        })
-    }
-
-    const getUser = async (login) => {
-        setLoading()
-
-
-        const reposnse = await fetch(`${GITHUB_URL}/users/${login}`, {
-            headers: {
-                Authorization : `token ${GITHUB_TOKEN}`,
-            },
-
-        })
-
-        if(Response.status === 404) {
-            window.location = '/notfound'
-        }else{
-            const data = await reposnse.json()
-
-            dispatch({
-                type:'GET_USER',
-                payload:data
-            })
-        }
-       
-        
-    }
-
-    const clearUsers = () => {
-        dispatch({
-            type:'CLEAR_USERS'
-        })
-    }
-
-    //set loading to true
-    const setLoading = () => dispatch({type:'SET_LOADING'})
+   
 
     return <GithubContext.Provider value={{
-        users: state.users,
-        loading: state.loading,
-        user:state.user,
-        searchUsers,
-        clearUsers,
-        getUser
+        ...state,
+        dispatch,
     }}>
     {children}
     </GithubContext.Provider>
